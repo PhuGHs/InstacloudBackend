d144e0b8c4da50d8615fef1ebc5aa855c3b2bc71
Phu Le <21522466@gm.uit.edu.vn>
2023-10-09 10:58:05 +0700

    reaction class utils

---
+++ src/features/reactions/models/reaction.schema.ts
@@ -0,0 +1,15 @@
+import { IReactionDocument } from '../interfaces/reaction.interface';
+import mongoose, { model, Model, Schema } from 'mongoose';
+
+const reactionSchema: Schema = new Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', index: true },
+  commentId: {type: mongoose.Schema.Types.ObjectId, ref: 'Comment', index: true },
+  username: { type: String, default: '' },
+  type: { type: String },
+  profilePicture: { type: String, default: '' },
+  createdAt: { type: Date, default: Date.now() }
+});
+
+const ReactionModel: Model<IReactionDocument> = model<IReactionDocument>('Reaction', reactionSchema, 'Reaction');
+
+export { ReactionModel };
