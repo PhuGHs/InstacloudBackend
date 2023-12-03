19f361fae7b49aa1706fc9d8c6d7bf4f47c639cb
Phu Le <21522466@gm.uit.edu.vn>
2023-12-03 15:46:02 +0700

    jest config and auth mock data

---
+++ src/auths/auth.mock.ts
@@ -0,0 +1,51 @@
+import { AuthPayload, IAuthDocument } from '@auth/interfaces/auth.interface';
+import { Response } from 'express';
+
+export const authMockRequest = (sessionData: IJWT, body: IAuthMock, currentUser?: AuthPayload | null, params?: any) => ({
+  session: sessionData,
+  body,
+  params,
+  currentUser
+});
+
+export const authMockResponse = (): Response => {
+  const res: Response = {} as Response;
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+export const authUserPayload: AuthPayload = {
+  userId: '60263f14648fed5246e322d9',
+  uId: '1621613119252066',
+  username: 'phule',
+  fullname: 'Phu Le',
+  email: 'manny@me.com',
+  iat: 12345
+};
+
+export const authMock = {
+  _id: '60263f14648fed5246e322d3',
+  uId: '1621613119252066',
+  username: 'phule',
+  fullname: 'Phu Le',
+  email: 'phule@gmail.com',
+  createdAt: '2022-08-31T07:42:24.451Z',
+  save: () => {},
+  comparePassword: () => false
+} as unknown as IAuthDocument;
+
+export interface IJWT {
+  jwt?: string,
+}
+
+export interface IAuthMock {
+  _id?: string;
+  username?: string;
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+  avatarImage?: string;
+  createdAt?: Date | string;
+}
