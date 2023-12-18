96893bff25389df7e8ebc4611eb7dbbc6405c20d
Phu Le <21522466@gm.uit.edu.vn>
2023-12-18 19:43:19 +0700

    change method patch to put

--- src/features/notifications/routes/notification.route.ts
+++ src/features/notifications/routes/notification.route.ts
@@ -10,7 +10,7 @@ class NotificationRoute {
     this.router = express.Router();
   }
   public routes(): Router {
-    this.router.patch('/notification/:notificationId', authMiddleware.checkAuthentication, Update.prototype.notification);
+    this.router.put('/notification/:notificationId', authMiddleware.checkAuthentication, Update.prototype.notification);
     this.router.get('/notification', authMiddleware.checkAuthentication, Get.prototype.notification);
     this.router.delete('/notification/:notificationId', authMiddleware.checkAuthentication, Delete.prototype.notification);
     return this.router;
