21acda98ed5c328fdea61100a73594d5ee72bcc0
Phu Le <21522466@gm.uit.edu.vn>
2023-12-20 20:18:13 +0700

    prepare for deployment

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,7 @@ class Config {
   public SECRET_KEY_TWO: string | undefined;
   public CLIENT_URL: string | undefined;
   public REDIS_HOST: string | undefined;
+  public REDIS_CLOUD: string | undefined;
   public CLOUD_NAME: string | undefined;
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
@@ -20,15 +21,6 @@ class Config {
   public SENDGRID_API_KEY: string | undefined;
   public SENDGRID_SENDER: string | undefined;
   public MONGODB_HOST: string | undefined;
-  // public MONGODB_USERNAME: string | undefined;
-  // public MONGODB_PASSWORD: string | undefined;
-  // public MONGODB_ATLAS_PROJECT_ID: string | undefined;
-  // public MONGODB_ATLAS_CLUSTER: string | undefined;
-  // public MONGODB_ATLAS_PUBLIC_KEY: string | undefined;
-  // public MONGODB_ATLAS_PRIVATE_KEY: string | undefined;
-  // public ATLAS_CLUSTER_API_URL: string | undefined;
-  // public ATLAS_SEARCH_INDEX_API_URL: string | undefined;
-  // public DIGEST_AUTH: string | undefined;
 
   public readonly ATLAS_API_BASE_URL = 'https://cloud.mongodb.com/api/atlas/v1.0';
   private readonly defaultDBURL: string = 'mongodb://localhost:27017/midcloud';
@@ -41,6 +33,7 @@ class Config {
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
     this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.REDIS_CLOUD = process.env.REDIS_CLOUD || '';
     this.CLOUD_NAME = process.env.CLOUD_NAME || '';
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
@@ -49,15 +42,6 @@ class Config {
     this.SENDGRID_SENDER = process.env.SENDGRID_SENDER || '';
     this.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
     this.MONGODB_HOST = process.env.MONGODB_HOST || '';
-    // this.MONGODB_USERNAME = process.env.MONGODB_USERNAME || '';
-    // this.MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || '';
-    // this.MONGODB_ATLAS_PROJECT_ID = process.env.MONGODB_ATLAS_PROJECT_ID || '';
-    // this.MONGODB_ATLAS_CLUSTER = process.env.MONGODB_ATLAS_CLUSTER || '';
-    // this.MONGODB_ATLAS_PUBLIC_KEY = process.env.MONGODB_ATLAS_PUBLIC_KEY || '';
-    // this.MONGODB_ATLAS_PRIVATE_KEY = process.env.MONGODB_ATLAS_PRIVATE_KEY || '';
-    // this.ATLAS_CLUSTER_API_URL = `${this.ATLAS_API_BASE_URL}/groups/${this.MONGODB_ATLAS_PROJECT_ID}/clusters/${this.MONGODB_ATLAS_CLUSTER}`;
-    // this.ATLAS_SEARCH_INDEX_API_URL = `${this.ATLAS_CLUSTER_API_URL}/fts/indexes`;
-    // this.DIGEST_AUTH = `${this.MONGODB_ATLAS_PUBLIC_KEY}:${this.MONGODB_ATLAS_PRIVATE_KEY}`;
   }
 
   public createLogger(name: string): bunyan {
